ce6bcc7d086084ada8179e354eb8fd56be43ab30
mrinmoy sarkar <mrinmoy.pol@gmail.com>
2018-10-27 21:02:02 -0400

    database updated
    Signed-off-by: mrinmoy sarkar <mrinmoy.pol@gmail.com>

--- gazebo-simulation-data/scenerioPlot.m
+++ gazebo-simulation-data/scenerioPlot.m
@@ -1,13 +1,17 @@
 clear all;
 close all;
 
-x=[0,0,1,1,0,0,1,1];
-y=[0,0,0,1,1,2,2,3];
-z=[0,1,1,1,1,1,1,1];
+x=[0,0,1,1,0,0,1,1,1,0,0];
+y=[0,0,0,1,1,2,2,3,3,0,0];
+z=[0,1,1,1,1,1,1,1,1,1,0];
 
 plot3(x,y,z);
-hold on
-x=[1,0,0];
-y=[3,0,0];
-z=[1,1,0];
-plot3(x,y,z)
+grid on
+xlabel('X position in meter')
+ylabel('Y position in meter')
+zlabel('Z position in meter')
+% hold on
+% x=[1,0,0];
+% y=[3,0,0];
+% z=[1,1,0];
+% plot3(x,y,z)
